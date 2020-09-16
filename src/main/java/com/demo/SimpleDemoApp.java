67d9e7b07f3f514ad6a4d6e9d7ba23a609bae6d0
psrkgouud@gmail.com <psrkgouud@gmail.com>
2020-09-16 09:13:54 +0000

    Testing

--- src/main/java/com/demo/SimpleDemoApp.java
+++ src/main/java/com/demo/SimpleDemoApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//Test
+//Test 123
 @SpringBootApplication
 //public class SimpleDemoAppApplication extends SpringBootServletInitializer {
 public class SimpleDemoApp extends SpringBootServletInitializer {
